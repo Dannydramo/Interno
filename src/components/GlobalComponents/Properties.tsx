73e11536c4ca088025b6871f3b610b09d94e1422
Dannydramo <alumandudaniel217@gmail.com>
2023-06-02 05:31:11 -0700

    initial commit

--- src/components/GlobalComponents/Properties.tsx
+++ src/components/GlobalComponents/Properties.tsx
@@ -16,38 +16,48 @@ const Properties: React.FC<hitImages> = ({
   hitPurpose,
 }) => {
   return (
-    <Link
-      to={
-        hitPurpose === "for-rent"
-          ? `/properties/for-rent/${hitImageId}`
-          : `/properties/for-sale/${hitImageId}`
-      }
-      className="flex flex-col"
-      key={hitImageId}
-    >
-      <img
-        src={hitImageCoverPhotoUrl}
-        className="rounded-3xl lg:h-[500px] w-full"
-      />
-      <div className="flex justify-between items-start mt-2">
-        <p className="font-bold mt-2">{hitImageAgencyName}</p>
+    <div className="overflow-x-hidden">
+      {" "}
+      <Link
+        to={
+          hitPurpose === "for-rent"
+            ? `/properties/for-rent/${hitImageId}`
+            : `/properties/for-sale/${hitImageId}`
+        }
+        className="flex flex-col"
+        key={hitImageId}
+      >
         <img
-          src={hitAgencyUrlLogo}
-          alt=""
-          className="w-[50px] h-[50px] rounded-full"
+          src={hitImageCoverPhotoUrl}
+          className="rounded-3xl lg:h-[500px] w-full"
         />
-      </div>
-      <p className="font-bold">
-        ${hitPrice}
-        {hitRentFrequency ? `/${hitRentFrequency}` : ""}
-      </p>
-      <p className="flex items-center">
-       <span className="flex items-center"> <span className="mx-2">{hitImageRooms}</span>
-        <FaBed /> </span> &nbsp;&nbsp;|<span className="flex items-center"><span className="mx-2">{hitImageBaths}</span>
-        <FaBath /></span>
-      </p>
-      <p className="truncate">{hitImageTitle}</p>
-    </Link>
+        <div className="flex justify-between items-start mt-2">
+          <p className="font-bold mt-2">{hitImageAgencyName}</p>
+          <img
+            src={hitAgencyUrlLogo}
+            alt=""
+            className="w-[50px] h-[50px] rounded-full"
+          />
+        </div>
+        <p className="font-bold">
+          ${hitPrice}
+          {hitRentFrequency ? `/${hitRentFrequency}` : ""}
+        </p>
+        <p className="flex items-center">
+          <span className="flex items-center">
+            {" "}
+            <span className="mx-2">{hitImageRooms}</span>
+            <FaBed />{" "}
+          </span>{" "}
+          &nbsp;&nbsp;|
+          <span className="flex items-center">
+            <span className="mx-2">{hitImageBaths}</span>
+            <FaBath />
+          </span>
+        </p>
+        <p className="truncate">{hitImageTitle}</p>
+      </Link>
+    </div>
   );
 };
 
